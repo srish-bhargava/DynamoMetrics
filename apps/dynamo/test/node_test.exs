f0aa6d47df6aeb46c6edac1049d81bf499224e91
disconsis <ketan.kanishka@nyu.edu>
2020-11-08 20:01:20 +0530

    add more tests targeting a single DynamoNode

--- apps/dynamo/test/node_test.exs
+++ apps/dynamo/test/node_test.exs
@@ -20,7 +20,7 @@ defmodule DynamoNodeTest do
     node_handle = Process.monitor(node)
 
     receive do
-      {:ok, value} ->
+      {_node, {:ok, value}} ->
         assert value == 42, "wrong value received for key"
 
       {:DOWN, ^node_handle, _, proc, reason} ->
@@ -28,6 +28,94 @@ defmodule DynamoNodeTest do
 
       msg ->
         assert false, "unexpected msg received: #{inspect(msg)}"
+    after
+      5_000 ->
+        assert false, "no message received"
+    end
+  after
+    Emulation.terminate()
+  end
+
+  test "Simple put request returns an :ok" do
+    Emulation.init()
+
+    node =
+      spawn(:node, fn ->
+        DynamoNode.start(:node, %{}, [:node], 1, 1, 1)
+      end)
+
+    node_handle = Process.monitor(node)
+
+    send(:node, {:put, :foo, 42})
+
+    receive do
+      {_node, :ok} ->
+        true
+
+      {:DOWN, ^node_handle, _, proc, reason} ->
+        assert false, "node #{inspect(proc)} crashed (reason: #{reason})"
+
+      msg ->
+        assert false, "unexpected msg received: #{inspect(msg)}"
+    after
+      5_000 ->
+        assert false, "no message received"
+    end
+  after
+    Emulation.terminate()
+  end
+
+  test "get after a put returns the put value with empty initial data" do
+    Emulation.init()
+
+    node =
+      spawn(:node, fn ->
+        DynamoNode.start(:node, %{}, [:node], 1, 1, 1)
+      end)
+
+    node_handle = Process.monitor(node)
+
+    send(:node, {:put, :foo, 42})
+
+    receive do
+      _ -> true
+    end
+
+    send(:node, {:get, :foo})
+
+    receive do
+      {:ok, value} ->
+        assert value == 42, "different value returned from :get"
+    after
+      5_000 ->
+        assert false, "no message received"
+    end
+  after
+    Emulation.terminate()
+  end
+
+  test "put request overwrites key in initial data" do
+    Emulation.init()
+
+    node =
+      spawn(:node, fn ->
+        DynamoNode.start(:node, %{foo: 37}, [:node], 1, 1, 1)
+      end)
+
+    node_handle = Process.monitor(node)
+
+    send(:node, {:put, :foo, 42})
+
+    receive do
+      _ -> true
+    end
+
+    send(:node, {:get, :foo})
+
+    receive do
+      {_node, {:ok, value}} ->
+        assert value != 37, "put did not overwrite key's value"
+        assert value == 42, "different value returned from :get"
     after
       5_000 ->
         assert false, "no message received"
