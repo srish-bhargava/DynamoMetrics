424c71f5adb96f9046cf1ce4ab4761d6031aeb1a
disconsis <ketan.kanishka@nyu.edu>
2020-11-08 17:56:39 +0530

    detail DynamoNode's outline a bit more

--- apps/dynamo/lib/node.ex
+++ apps/dynamo/lib/node.ex
@@ -3,11 +3,25 @@ defmodule DynamoNode do
   A replica node in a dynamo cluster.
   """
 
+  alias ExHashRing.HashRing
+
+  # override Kernel's functions with Emulation's
+  import Emulation, only: [spawn: 2, send: 2, timer: 1, now: 0, whoami: 0]
+
+  import Kernel,
+    except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
+
+  require Fuzzers
+  require Logger
+
+  # The state of each node
   defstruct(
     # node id
     id: nil,
     # local storage of key-(value, clock) pairs
     store: nil,
+    # all nodes in the cluster
+    nodes: nil,
     # hash ring
     ring: nil,
     # parameters from dynamo
@@ -20,16 +34,69 @@ defmodule DynamoNode do
   )
 
   @doc """
-  Start the node.
-  def start(id, store, nodes, n, r, w) do
-
-  Sets up its state,
-  starts bg threads for anti-entropy protocol
-  then listens for :get and :put requests in a loop
-  (updating vector_clock as it goes)
+  Set up node and start serving requests.
   """
-  def start(id, store, nodes) do
-    raise "TODO"
+  def start(id, store, nodes, n, r, w) do
+    Logger.info("Starting node #{inspect(id)}")
+
+    state = %DynamoNode{
+      id: id,
+      store: store,
+      nodes: nodes,
+      ring: HashRing.new(nodes, 1),
+      n: n,
+      r: r,
+      w: w,
+      vector_clock: VectorClock.new(nodes)
+    }
+
+    # TODO start anti-entropy in a background process
+
+    listener(state)
+  end
+
+  @doc """
+  Listen and serve requests, forever.
+  """
+  def listener(state) do
+    receive do
+      # client requests
+      {client, {:get, key}} ->
+        Logger.info(
+          "Received a :get request for key=#{inspect(key)} " <>
+            "from client=#{inspect(client)}"
+        )
+
+        raise "TODO"
+
+      {client, {:put, key, value}} ->
+        Logger.info(
+          "Received a :put request for key=#{inspect(key)}, " <>
+            "value=#{inspect(value)} from client=#{inspect(client)}"
+        )
+
+        raise "TODO"
+
+      # internal requests
+      {coordinator, {:get_internal, key}} ->
+        Logger.info(
+          "Received a :get_internal request for key=#{inspect(key)} " <>
+            "from coordinator=#{inspect(coordinator)}"
+        )
+
+        raise "TODO"
+
+      {coordinator, {:put_internal, key, value, clock}} ->
+        Logger.info(
+          "Received a :put_internal request for key=#{inspect(key)}, " <>
+            "value=#{inspect(value)}, clock=#{inspect(clock)} from " <>
+            "coordinator=#{inspect(coordinator)}"
+        )
+
+        raise "TODO"
+    end
+
+    listener(state)
   end
 
   @doc """
@@ -40,9 +107,9 @@ defmodule DynamoNode do
   1. Request all versions of data from the top `n` nodes in
        the preference list for key (regardless of whether
        we believe them to be healthy or not).
-  3. If multiple versions gathered, remove older ones.
-  4. Return all unrelated versions of data.
   2. Wait for r responses.
+  3. Return all latest concurrent versions of the key's values
+       received.
   """
   def get(state, key) do
     raise "TODO get"
