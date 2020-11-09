15a7dcda39a2e1fa796ed39011054638f7c0895c
disconsis <ketan.kanishka@nyu.edu>
2020-11-09 09:24:56 +0530

    add name for project so we can generate docs

--- mix.exs
+++ mix.exs
@@ -3,6 +3,7 @@ defmodule DynamoMetrics.MixProject do
 
   def project do
     [
+      name: "DynamoMetrics",
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
